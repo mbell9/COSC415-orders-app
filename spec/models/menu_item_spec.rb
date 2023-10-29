8cead02a2140a186f00c0fee4207a5618acf09bc
“zahmed333” <zahmed@colgate.edu>
2023-10-29 15:55:24 -0400

    Added more tests to MenuItems

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -6,4 +6,23 @@ RSpec.describe MenuItem, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:price) }
   it { should validate_numericality_of(:price) }
+
+  it "is valid with valid attributes" do
+    expect(build(:menu_item)).to be_valid
+  end
+
+  it "is not valid without a name" do
+    menu_item = build(:menu_item, name: nil)
+    expect(menu_item).not_to be_valid
+  end
+
+  it "is not valid without a price" do
+    menu_item = build(:menu_item, price: nil)
+    expect(menu_item).not_to be_valid
+  end
+
+  it "belongs to a restaurant" do
+    association = described_class.reflect_on_association(:restaurant)
+    expect(association.macro).to eq :belongs_to
+  end
 end
