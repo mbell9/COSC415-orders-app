e2f997aeee29705c7a5a213e32822c1a9e0ca895
mbell9 <mbell17bay@gmail.com>
2023-10-30 12:19:48 -0400

    browse setup MVC

---
+++ app/controllers/browse_controller.rb
@@ -0,0 +1,14 @@
+class BrowseController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    begin
+        @restaurant = Restaurant.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        redirect_to restaurants_path
+    end
+  end
+
+end
