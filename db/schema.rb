4604215c8de608dc9e2789481a34606c5c8e834f
NBuji <nbujiashvili@colgate.edu>
2023-10-31 01:01:26 -0400

    V1, Reviews

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,8 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-#wael schema
-#ActiveRecord::Schema[7.1].define(version: 2023_10_30_003510) do
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_30_050408) do
-  
+ActiveRecord::Schema[7.1].define(version: 2023_10_30_232058) do
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "phone_number"
@@ -22,7 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_30_050408) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "menu_items", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
