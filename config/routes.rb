8ae1983a53e2d5f2abefb7624bc7540a38becd2d
zahmed333 <zahmed@colgate.edu>
2023-11-13 00:41:55 -0500

    User Auth done, fixed migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,28 @@
 Rails.application.routes.draw do
-  get 'customers/show'
-  get 'customers/edit'
-  get 'customers/update'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Devise routes for User
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 
-# Health check route
-get "up" => "rails/health#show", as: :rails_health_check
+  # Health check route
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
-# Root route
-root 'browse#index'
+  # Root route
+  root 'browse#index'
 
-# Browse routes
-resources :browse, only: [:index, :show]
-get 'restaurants', to: 'browse#index'
-get 'restaurants/:id', to: 'browse#show', as: :restaurant
-
-# Restaurant routes with nested resources for menu_items and reviews
-resources :restaurants do
-  resources :menu_items, only: [:index, :new, :create, :edit, :update, :destroy]
-  resources :reviews, only: [:index, :new, :create, :edit, :update, :destroy]
-end
-
-# Customer routes
-resources :customers, only: [:show, :edit, :update]
-
-# MenuItems routes
-#resources :menu_items, only: [:show, :edit, :update, :destroy]
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Browse routes
+  resources :browse, only: [:index, :show]
+  
+  # Restaurant routes with nested resources for menu_items and reviews
   resources :restaurants do
-    resources :menu_items, only: [:index, :new, :create]
+    resources :menu_items, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :reviews, only: [:index, :new, :create, :edit, :update, :destroy]
   end
 
+  # Customer routes
+  resources :customers, only: [:show, :edit, :update]
+
+  # Cart routes with nested cart_items
   resources :carts, only: [:show] do
     resources :cart_items, only: [:create, :destroy] do
       member do
@@ -41,5 +31,4 @@ resources :customers, only: [:show, :edit, :update]
       end
     end
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_30_061440) do
-  create_table "cart_items", force: :cascade do |t|
-    t.integer "cart_id", null: false
-    t.integer "menu_item_id", null: false
-    t.integer "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
-    t.index ["menu_item_id"], name: "index_cart_items_on_menu_item_id"
-  end
-
-  create_table "carts", force: :cascade do |t|
-    t.integer "customer_id", null: false
-    t.integer "restaurant_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["customer_id"], name: "index_carts_on_customer_id"
-    t.index ["restaurant_id"], name: "index_carts_on_restaurant_id"
-    
-ActiveRecord::Schema[7.1].define(version: 2023_11_09_200950) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_13_034424) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -58,9 +39,27 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_09_200950) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "cart_id", null: false
+    t.integer "menu_item_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["menu_item_id"], name: "index_cart_items_on_menu_item_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_carts_on_customer_id"
+    t.index ["restaurant_id"], name: "index_carts_on_restaurant_id"
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.string "phone_number"
     t.string "email"
     t.string "address"
@@ -95,11 +94,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_09_200950) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "cart_items", "carts"
-  add_foreign_key "cart_items", "menu_items"
-  add_foreign_key "carts", "customers"
-  add_foreign_key "carts", "restaurants"
-
   create_table "reviews", force: :cascade do |t|
     t.date "recorded_date"
     t.integer "rating"
@@ -112,8 +106,24 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_09_200950) do
     t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "menu_items"
+  add_foreign_key "carts", "customers"
+  add_foreign_key "carts", "restaurants"
   add_foreign_key "menu_items", "restaurants"
   add_foreign_key "reviews", "customers"
   add_foreign_key "reviews", "restaurants"
