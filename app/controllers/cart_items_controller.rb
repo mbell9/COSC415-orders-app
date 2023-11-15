a346f1ea132c9c46bb2effeece2e41e21232d272
lleaipailote <lleaipailote@colgate.edu>
2023-11-15 01:29:36 -0500

    cart now only adds item from the same restaurant

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -21,16 +21,24 @@ class CartItemsController < ApplicationController
     def add_to_cart
       menu_item = MenuItem.find(params[:menu_item_id])
       cart_item = @cart.cart_items.find_or_initialize_by(menu_item: menu_item)
-      if cart_item.new_record?
-        cart_item.quantity = 1
-      else
-        cart_item.quantity += 1
-      end
 
-      if cart_item.save
-        redirect_to @cart, notice: 'Item quantity increased.'
+      if params[:set_restaurant_id] && @cart.restaurant_id.nil?
+        @cart.restaurant_id = menu_item.restaurant_id
+        @cart.save
+      elsif params[:set_restaurant_id] && @cart.restaurant_id != menu_item.restaurant_id
+          redirect_to customer_menu_path(restaurant_id: menu_item.restaurant_id), notice: 'You have cart items from another restaurant' and return
       else
-        redirect_to @cart, alert: 'Failed to increase item quantity.'
+        if cart_item.new_record?
+          cart_item.quantity = 1
+        else
+          cart_item.quantity += 1
+        end
+
+        if cart_item.save
+          redirect_to @cart, notice: 'Item quantity increased.'
+        else
+          redirect_to @cart, alert: 'Failed to increase item quantity.'
+        end
       end
     end
   
