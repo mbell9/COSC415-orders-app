3a3de1832eaa0d8c249a0383cc893b344e237d6d
Wael Mohamed <wmohamed@colgate.edu>
2023-12-07 21:28:44 -0500

    user model and mailer

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,40 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'new_order_email' do
+    let(:order) { FactoryBot.create(:order) }
+
+    it 'sends a new order email' do
+      email = UserMailer.new_order_email(order)
+
+      expect(email.to).to eq([order.restaurant.user.email])
+      expect(email.subject).to eq('You received a new order!')
+      expect(email.body.encoded).to match(order.id.to_s) # Example of checking body content
+    end
+  end
+
+  describe 'placed_order_email' do
+    let(:order) { FactoryBot.create(:order) }
+
+    it 'sends a placed order email' do
+      email = UserMailer.placed_order_email(order)
+
+      expect(email.to).to eq([order.customer.user.email])
+      expect(email.subject).to eq('You placed an order successfully!')
+      expect(email.body.encoded).to match(order.id.to_s)
+    end
+  end
+
+  describe 'status_update_email' do
+    let(:order) { FactoryBot.create(:order) }
+    let(:message) { "Your order status has been updated" }
+
+    it 'sends a status update email' do
+      email = UserMailer.status_update_email(order, message)
+
+      expect(email.to).to eq([order.customer.user.email])
+      expect(email.subject).to eq(message)
+      expect(email.body.encoded).to match(order.id.to_s)
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -89,4 +89,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  SimpleCov.start 'rails' do
+    add_filter '/app/controllers/users/'
+  end
 end
